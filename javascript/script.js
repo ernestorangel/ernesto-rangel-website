eb72cbe7f56f59179f002e3fbecdc2ff9ef4bb70
ernestorangel <ernestojrcjunior@gmail.com>
2023-04-04 21:15:49 -0300

    wip

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,18 @@
-function testingNeoVim() {
-	console.log("testing neovim");
-};
+let userLang = navigator.language || navigator.userLanguage;
+let langSelector = document.getElementById('lang-selector');
+
+window.addEventListener('load', () => {
+  setLanguage(userLang);
+
+  langSelector.addEventListener('input', () => {
+    setLanguage(langSelector.value);
+  });
+});
+
+function setLanguage(langCode) {
+  fetch(`../lang/${langCode}.json`)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function setLanguageSelector(langCode) {}
